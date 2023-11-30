88a280a0085344d19a90635adb4a5f0ae33870d6
Yoga <im.brindawan@gmail.com>
2023-12-01 00:23:59 +0700

    V0.1

--- CinemaXXII/src/cinemaxxii/CinemaXXII.java
+++ CinemaXXII/src/cinemaxxii/CinemaXXII.java
@@ -8,13 +8,31 @@ package cinemaxxii;
  *
  * @author Yoga
  */
+import java.util.List;
 public class CinemaXXII {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Movie movie1 = new Movie("Inception", "Sci-Fi", 150);
+        Movie movie2 = new Movie("The Shawshank Redemption", "Drama", 142);
+
+        Studio studio1 = new Studio("Studio A", 15);
+        Studio studio2 = new Studio("Studio B", 10);
+
+        studio1.addMovie(movie1);
+        studio2.addMovie(movie2);
+
+        studio1.reserveSeat(6);
+        studio1.reserveSeat(8);
+        studio1.reserveSeat(11);
+
+        studio2.reserveSeat(4);
+        studio2.reserveSeat(8);
+
+        Theater theater = new Theater(List.of(studio1, studio2));
+        theater.displayInfo();
     }
     
 }
---
+++ CinemaXXII/src/cinemaxxii/Movie.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cinemaxxii;
+
+/**
+ *
+ * @author Yoga
+ */
+public class Movie {
+    private String title;
+    private String genre;
+    private int duration;
+
+    public Movie(String title, String genre, int duration) {
+        this.title = title;
+        this.genre = genre;
+        this.duration = duration;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+}
