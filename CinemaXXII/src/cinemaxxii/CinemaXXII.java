616522c06fbfb383a9056e3c5f8288ce60ebfae9
Yoga <im.brindawan@gmail.com>
2023-12-15 13:13:09 +0700

    Feat : Customer page

--- CinemaXXII/src/cinemaxxii/CinemaXXII.java
+++ CinemaXXII/src/cinemaxxii/CinemaXXII.java
@@ -8,48 +8,23 @@ package cinemaxxii;
  *
  * @author Yoga
  */
-<<<<<<< Updated upstream
-=======
-import cinemaxxii.view.Display;
-import java.util.List;
->>>>>>> Stashed changes
+
+
+import cinemaxxii.display.DisplayFrame;
+
 public class CinemaXXII {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-<<<<<<< Updated upstream
-        // TODO code application logic here
-=======
-        // Membuat objek User
-        User user1 = new User("John Doe", "john.doe@example.com");
+
+//        Home home = new Home();
+//        home.setVisible(true);
         
-        //Frame login & register
-        Display gateFrame = new Display();
-        
-        gateFrame.setVisible(true);
+        DisplayFrame display = new DisplayFrame();
+        display.setVisible(true);
 
-        // Membuat objek Movie
-        Movie movie1 = new Movie("Inception", "Sci-Fi", 150);
-
-        // Membuat objek Studio
-        Studio studio1 = new Studio("Studio A", 15);
-        Studio studio2 = new Studio("Studio B", 10);
-
-        // Menambahkan film ke studio
-        studio1.addMovie(movie1);
-
-        // Reservasi kursi dengan informasi pengguna
-        studio1.reserveSeat(6, user1);
-
-        // Menampilkan informasi reservasi
-        studio1.getReservationInfo(6);
-
-        // Menampilkan informasi teater
-        Theater theater = new Theater(List.of(studio1, studio2));
-        theater.displayInfo();
->>>>>>> Stashed changes
     }
     
 }
