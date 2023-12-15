4330e36fa075802213d1ee6a47cfc6174988c71c
AndreMasalle <114896027+AndreMasalle@users.noreply.github.com>
2023-12-15 13:32:38 +0700

    Fix : Java version

--- AdminCinemaXXII/src/admincinemaxxii/AdminCinemaXXII.java
+++ AdminCinemaXXII/src/admincinemaxxii/AdminCinemaXXII.java
@@ -15,6 +15,7 @@ public class AdminCinemaXXII {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Halo warudo");
     }
     
 }
